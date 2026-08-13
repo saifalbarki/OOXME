b311488a1e41c86768c0e63ebf3eaee5c5e77e65
Saif Albarki <saif6aresh@gmail.com>
2026-08-13 14:27:48 +0300

    Include site metadata in static output

--- scripts/build-static.js
+++ scripts/build-static.js
@@ -5,6 +5,7 @@ const root = path.resolve(__dirname, '..');
 const sourceRoot = root;
 const output = path.join(root, 'dist');
 const directories = ['assets', 'css', 'js', 'public'];
+const rootFiles = ['favicon.svg', 'site.webmanifest'];
 
 if (!fs.existsSync(sourceRoot)) {
   throw new Error('Expected website source directory is missing.');
@@ -24,6 +25,11 @@ for (const name of directories) {
   if (fs.existsSync(source)) {
     fs.cpSync(source, path.join(output, name), { recursive: true });
   }
+}
+
+for (const name of rootFiles) {
+  const source = path.join(sourceRoot, name);
+  if (fs.existsSync(source)) fs.copyFileSync(source, path.join(output, name));
 }
 
 console.log('Static deployment files created in dist.');
